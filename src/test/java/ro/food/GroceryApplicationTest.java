033b34d42c66bfb160a8dae12013bbdd174ed433
AlinaAlu <alina_uricaru@yahoo.com>
2023-04-05 13:57:40 +0300

    Create project structure

---
+++ src/test/java/ro/food/GroceryApplicationTest.java
@@ -0,0 +1,15 @@
+package ro.food;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GroceryApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
